6299ca5b448cdbeea00125b04b54a15f36816b8c
Andrew Wason <rectalogic@rectalogic.com>
2024-09-28 17:56:25 -0400

    handle different output formats. add dependencies

--- README.md
+++ README.md
@@ -15,7 +15,73 @@ llm install llm-transformers
 ```
 ## Usage
 
-Usage instructions go here.
+Models that generate audio will save the audio to a file, the pathname is the output of the `llm` command.
+Some models can also be parameterized with keyword arguments.
+```sh-session
+$ llm -m transformers -o kwargs '{"generate_kwargs": {"max_new_tokens": 100}}' -o model facebook/musicgen-small "techno music"
+/var/folders/b1/1j9kkk053txc5krqbh0lj5t00000gn/T/tmpoueh05y6.wav
+```
+
+Some models can take a URL or path to a file as input, for example:
+```sh-session
+$ llm -m transformers -o task audio-classification https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/1.flac
+[
+    {
+        "score": 0.9972336888313293,
+        "label": "_unknown_"
+    },
+    {
+        "score": 0.0019911774434149265,
+        "label": "left"
+    },
+    {
+        "score": 0.0003051063104066998,
+        "label": "yes"
+    },
+    {
+        "score": 0.0002108386834152043,
+        "label": "down"
+    },
+    {
+        "score": 0.00011406492558307946,
+        "label": "stop"
+    }
+]
+```
+```sh-session
+$ llm -m transformers -o task automatic-speech-recognition https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/1.flac
+HE HOPED THERE WOULD BE STEW FOR DINNER TURNIPS AND CARROTS AND BRUISED POTATOES AND FAT MUTTON PIECES TO BE LADLED OUT IN THICK PEPPERED FLOWER FAT AND SAUCE
+```
+```sh-session
+$ llm -m transformers -o task sentiment-analysis "We are very happy to show you the 🤗 Transformers library"
+[
+    {
+        "label": "POSITIVE",
+        "score": 0.9997681975364685
+    }
+]
+```
+Some pipeline tasks accept an image url or path as input and generate an image file as output:
+```sh-session
+$ llm -m transformers -o task depth-estimation http://images.cocodataset.org/val2017/000000039769.jpg
+/var/folders/b1/1j9kkk053txc5krqbh0lj5t00000gn/T/tmpjvp9uo7x.png
+```
+XXX embed image here?
+
+Some pipeline tasks require specific kwarg options:
+```sh-session
+$ llm -m transformers -o task document-question-answering -o kwargs '{"image": "https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png"}' "What is the invoice number?"
+us-001
+```
+
+```sh-session
+$ llm -m transformers -o task fill-mask "My <mask> is about to explode"
+My brain is about to explode (score=0.09140042215585709)
+My heart is about to explode (score=0.07742168009281158)
+My head is about to explode (score=0.05137857422232628)
+My fridge is about to explode (score=0.029346412047743797)
+My house is about to explode (score=0.02866862528026104)
+```
 
 ## Development
 
--- llm_transformers.py
+++ llm_transformers.py
@@ -1,12 +1,15 @@
 import typing as ta
+import tempfile
 import json
 import click
 from pydantic import field_validator, model_validator, Field
+import soundfile as sf
 import llm
 import torch
 from transformers.pipelines import get_supported_tasks
 from transformers.utils import get_available_devices
 from transformers import pipeline
+import PIL
 
 # XXX get list of models? https://github.com/huggingface/transformers/blob/2e24ee4dfa39cc0bc264b89edbccc373c8337086/src/transformers/models/auto/configuration_auto.py#L637
 # XXX allow local paths?
@@ -43,42 +46,54 @@ class Transformers(llm.Model):
 
     class Options(llm.Options):
         task: str | None = Field(
-            description="Transformer task name. `llm transformers list-tasks`.",
+            description="Transformer pipeline task name. `llm transformers list-tasks`.",
             default=None
         )
         model: str | None = Field(
             description="Transformer model name.",
             default=None
         )
+        kwargs: dict | None = Field(
+            description="Pipeline keyword args JSON dict. Specify additional kwargs for some pipelines.",
+            default=None
+        )
         device: str | None = Field(
             description="Device name. `llm transformers list-devices`.",
             default=None
         )
-        # model: str | None = None
-        # XXX add tokenizer, device (cpu/gpu)
 
+        @field_validator("kwargs", mode='before')
+        @classmethod
+        def validate_kwargs(cls, kwargs) -> dict | None:
+            if kwargs is None or isinstance(kwargs, dict):
+                return kwargs
+            d = json.loads(kwargs)
+            if not isinstance(d, dict):
+                raise ValueError("Invalid pipeline kwargs JSON option.")
+            return d
+    
         @field_validator("task")
         @classmethod
-        def validate_task(cls, task) -> ta.Any:
+        def validate_task(cls, task) -> str | None:
             if task is None:
                 return None
             if task not in get_supported_tasks():
-                raise ValueError("Invalid task name.")
+                raise ValueError("Invalid pipeline task name option.")
             return task
 
         @field_validator("device")
         @classmethod
-        def validate_device(cls, device) -> ta.Any:
+        def validate_device(cls, device) -> str | None:
             if device is None:
                 return None
             if device not in get_available_devices():
-                raise ValueError("Invalid device name.")
+                raise ValueError("Invalid device name option.")
             return device
 
         @model_validator(mode='after')
         def check_task_model(self) -> ta.Self:
             if self.task is None and self.model is None:
-                raise ValueError("Must specify task and/or model.")
+                raise ValueError("Must specify pipeline task and/or model options.")
             return self
 
     def execute(self,
@@ -86,10 +101,53 @@ class Transformers(llm.Model):
         stream: bool,
         response: llm.Response,
         conversation: llm.Conversation | None =None,
-    ):
-        #XXX hrm, passing model alone only works if it is already associated with a task - seems ok, e.g. llama3-3.1-8B has pipeline_tag text-generation
-        pipe = pipeline(task=prompt.options.task, model=prompt.options.model, device=torch.device(prompt.options.device) if prompt.options.device is not None else None)
-        result = pipe(prompt.prompt)
-        if not isinstance(result, str):
-            return json.dumps(result)
-        return result
+    ) -> ta.Iterator[str]:
+        pipe = pipeline(
+            task=prompt.options.task,
+            model=prompt.options.model,
+            device=torch.device(prompt.options.device) if prompt.options.device is not None else None,
+        )
+
+        args = []
+        kwargs = prompt.options.kwargs if prompt.options.kwargs is not None else {}
+        if pipe.task == "document-question-answering":
+            if "image" not in kwargs:
+                raise ValueError("Must specify 'image' path/URL kwargs option for document-question-answering pipeline task.")
+            kwargs["question"] = prompt.prompt
+        else:
+            args.append(prompt.prompt)
+
+        result = pipe(*args, **kwargs)
+
+        match result:
+            case str():
+                yield result
+            case {"text": text}:  # automatic-speech-recognition
+                response.response_json = {pipe.task: result}
+                yield text
+            case {"audio": audio, "sampling_rate": sampling_rate}:  # text-to-audio
+                with tempfile.NamedTemporaryFile(suffix=".wav", delete=False, delete_on_close=False) as f:
+                    # musicgen is shape (batch_size, num_channels, sequence_length)
+                    # https://huggingface.co/docs/transformers/v4.45.1/en/model_doc/musicgen#unconditional-generation
+                    #XXX check shape of other audio pipelines
+                    sf.write(f, audio[0].T, sampling_rate)
+                    response.response_json = {pipe.task: {"output": f.name}}
+                    yield f.name
+            case [{"score": float(), "label": str()}, *_]:  # audio-classification, sentiment-analysis
+                response.response_json = {pipe.task: result}
+                yield json.dumps(result, indent=4)
+            case [{"sequence": str(), "token": int(), "token_str": str(), "score": float()}, *_]:  # fill-mask
+                response.response_json = {pipe.task: result}
+                yield "\n".join(f"{item['sequence']} (score={item['score']})" for item in result)
+            case {"predicted_depth": _, "depth": depth}:  # depth-estimation
+                with tempfile.NamedTemporaryFile(suffix=".png", delete=False, delete_on_close=False) as f:
+                    depth.save(f, format="png")
+                    response.response_json = {pipe.task: {"output": f.name}}
+                    yield f.name
+            case [{"score": float(), "answer": str(), "start": int(), "end": int()}]:  # document-question-answering
+                response.response_json = {pipe.task: result}
+                yield result[0]["answer"]
+            case _:
+                breakpoint()
+                print("DEFAULT CASE") #XXX
+                yield json.dumps(result, indent=4)
